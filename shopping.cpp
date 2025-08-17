f176f53f9f7c6e8bbba4e484dc152c3bce3ee05f
Hariprasath Krishnamoorthi <hariprasath.krishnamoorthi@psiog.com>
2025-08-17 04:00:29 -0700

    Initial commit

---
+++ shopping.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void processItem(string item) {
+  
+}
+void goodsAndOrders() {
+
+}
+
+int main() {
+    cout << "    Order Processing System    \n";
+
+    // int n;
+    // cout << "Enter number of items in the order: ";
+    // cin >> n;
+
+    // vector<string> items(n);
+    // for (int i = 0; i < n; i++) {
+    //     cout << "Enter name of item " << i + 1 << ": ";
+    //     cin >> items[i];
+    // }
+
+    cout << "\nOrder placed successfully.\n";
+
+    for (int i = 0; i < n; i++) {
+        processItem(items[i]); 
+    }
+    goodsAndOrders();
+    cout << "\nOrder process completed. STOP.\n";
+    return 0;
+}
